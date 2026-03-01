a5a7200cc8669b71c2ee7c74a5f9678ecd62657a
Slookeur <sebastien.leroux@ipcms.unistra.fr>
2026-03-01 13:54:34 +0100

    Bonds sources: dim. 01 mars 2026 13:54:34 CET

--- c_to_a.sh
+++ c_to_a.sh
@@ -15,7 +15,7 @@ function clean {
   sed 's/&/\\&/g' tmp_b > tmp_a
   sed 's/#/\\#/g' tmp_a > tmp_b
 
-  sed 's/\\begin{algorithm}/\\begin{algorithm}\n\\begin{algorithmic}\\Procedure{}/g' tmp_b > tmp_a
+  sed 's/\\begin{algorithm}/\\begin{algorithm}\n\\begin{algorithmic}\n\\Procedure{}/g' tmp_b > tmp_a
   sed 's/\\end{algorithm}/\\EndProcedure\n\\end{algorithmic}\n\\end{algorithm}/g' tmp_a > tmp_b
 
   #sed 's/|\\zero|/0/g' tmp_b > tmp_a
