31b1b8acde40088c56641e93007ad81df37e64c9
Slookeur <sebastien.leroux@ipcms.unistra.fr>
2026-03-01 13:51:10 +0100

    Bonds sources: dim. 01 mars 2026 13:51:10 CET

--- c_to_a.sh
+++ c_to_a.sh
@@ -38,7 +38,7 @@ function clean {
   #sed 's/|\\rbtt{ypos}|/ypos/g' tmp_a > tmp_b
   #sed 's/|\\obtt{zpos}|/zpos/g' tmp_b > tmp_a
   #sed 's/%\/\//\/\//g' tmp_a > tmp_b
-  sed 's/|\\textquotesingle|/{\\textquotesingle}/g' tmp_b > tmp_a
+  sed 's/\\textquotesingle/{\\textquotesingle}/g' tmp_b > tmp_a
   #sed 's/|\\dbtt{axis}|/axis/g' tmp_a > tmp_b
   # sed 's/\\AA\\/Angstrom/g' tmp_a > tmp_b
 
